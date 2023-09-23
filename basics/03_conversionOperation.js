089b790e196862b0acfc6deab9532c574551259f
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-09-23 08:48:43 +0000

    comparision

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -27,21 +27,23 @@ let value=3
 let negValue=-value
 //console.log(negValue);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(5%2);
-// console.log(2**3);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(5%2);
+console.log(2**3);
 
 let str1="hello"
 let str2=" Sarita"
 let str3=str1 + str2
-console.log(str3);
+// console.log(str3);
 
 console.log("1" + 2 + 2);//122
 console.log(1+2+"2");//32
 console.log(1 + "2");//12
 console.log("2" + 1);//21
+console.log((3+4) * 5 % 3)
+console.log(+true);
 
 
 
