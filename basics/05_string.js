0a65bcf8389c9d9670a0ec6e03e370c43aeeed28
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-09-23 10:32:21 +0000

    String concept

---
+++ basics/05_string.js
@@ -0,0 +1,33 @@
+const name="Sarita"
+const repoCount=60
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo cont is ${repoCount}`);
+
+const gameName=new String('sarita-hc-com')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+
+const newStringOne= "   sarita  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+const url ="https://sarita.com/sarita%20maurya"
+
+
+console.log(url.replace('%20','-'));
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
