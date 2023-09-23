72a7515f8a8cbf9f2c54cb8113d92cd10c904e8d
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-09-23 06:39:22 +0000

    Operations

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -1,21 +1,65 @@
-let score="33"
-console.log(typeof score);
-console.log(typeof(score));
+// let score="33"
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
 
-// "333"=> 33
-// 33 =>NaN
-// undefined  => NaN
-// true => 1
-//false =>0
-// null => NaN
+// // "333"=> 33
+// // 33 =>NaN
+// // undefined  => NaN
+// // true => 1
+// //false =>0
+// // null => NaN
 
 
-let isLoggedIn="Sarita Maurya"
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn="Sarita Maurya"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // "" => false
 //"sarita maurya"=>true
+
+//***************************  Operation ************************
+
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(5%2);
+// console.log(2**3);
+
+let str1="hello"
+let str2=" Sarita"
+let str3=str1 + str2
+console.log(str3);
+
+console.log("1" + 2 + 2);//122
+console.log(1+2+"2");//32
+console.log(1 + "2");//12
+console.log("2" + 1);//21
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
