265dcc5de479168bac666b097f86f266cc6d21fd
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-09-23 06:23:04 +0000

    conversionOperation

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+
+// "333"=> 33
+// 33 =>NaN
+// undefined  => NaN
+// true => 1
+//false =>0
+// null => NaN
+
+
+let isLoggedIn="Sarita Maurya"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// "" => false
+//"sarita maurya"=>true
