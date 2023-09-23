84516d1a6dfc0530fb8007a1c3bdef0ea4a776be
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-09-23 09:37:18 +0000

    datatype-summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive data type
+ //  String, Number, Boolean , null , undefined , Symbol , BigInt
+
+ const score=100
+ const scoreValue= 100.2
+  
+ const isLoggedIn=false
+ const outsideTemp=null
+ let useEmail
+
+ const id=Symbol('123')
+ const anotherId=Symbol('123')
+ console.log(id===anotherId);
+
+ // Non Primitive data type
+
+// Array, Object , Function
+
+const heros=["Sarita" , "Akash", "shambhu"];
+let myObj={
+    name:"Sarita",
+    age:22,
+}
+
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+
+
+
+
+
