bce7d2ac83caf92fe48430d5f634042466097364
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-09-24 11:46:44 +0000

    num & Math

---
+++ basics/06_nums_and_math.js
@@ -0,0 +1,25 @@
+const score=400
+// console.log(score);
+
+const balance=100
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber=123.8967
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds=1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// **************** Maths *********************
+console.log(Math);
+console.log(Math.round(5.9));
+
+
+
+
+
+
