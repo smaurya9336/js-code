44da6b815c431ced870faf7353d1daa7a848faf2
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-09-25 09:58:01 +0000

    randon function

--- basics/06_nums_and_math.js
+++ basics/06_nums_and_math.js
@@ -15,11 +15,22 @@ const hundreds=1000000
 // console.log(hundreds.toLocaleString('en-IN'));
 
 // **************** Maths *********************
-console.log(Math);
-console.log(Math.round(5.1));
-console.log(Math.ceil(4.0));
-console.log(Math.floor(5.5));
-console.log("hello world");
+// console.log(Math);
+// console.log(Math.round(5.1));
+// console.log(Math.ceil(4.0));
+// console.log(Math.floor(5.5));
+// console.log("hello world");
+// console.log(Math.min(4,76,36));
+// console.log(Math.max(65,57,90));
+console.log(Math.random());
+console.log(Math.random()*10 +1);
+console.log(Math.floor(Math.random() *10)+1);
+const min=10
+const max=20
+
+
+console.log(Math.floor(Math.random()*(max - min +1))+min);
+console.log(Math.floor(Math.random()*(max - min +1))+max);
 
 
 
