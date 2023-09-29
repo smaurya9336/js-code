89cfe30807014d95eacc4544012e7518a6f0d307
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-09-29 09:01:58 +0000

    array

--- basics/08_array.js
+++ basics/08_array.js
@@ -2,4 +2,24 @@
 
 
 const myArr=[0,1,2,3,45,true,"sarita"]
-console.log(myArr);
+// console.log(myArr);
+// console.log("sarita");
+// myArr.pop()
+// myArr.push(7)
+// console.log(myArr);
+// myArr.unshift(9)
+// myArr.shift()
+
+const newArr=myArr.join()
+console.log(newArr);
+console.log(typeof newArr);
+
+
+// slice,splice
+const myn1=myArr.slice(1,3)
+console.log("A",myArr);
+console.log(myn1);
+
+const myn2=myArr.splice(1,3)
+console.log("B",myArr)
+console.log(myn2);
