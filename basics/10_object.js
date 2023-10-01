d3c743c9c6cfc6670a2cef82e044e7555f6482cb
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-10-01 15:06:41 +0000

    object

--- basics/10_object.js
+++ basics/10_object.js
@@ -2,17 +2,33 @@
 
 
 //object literals
-const jsUser={
-    name:"sarita",
-    age:22,
-    email:"sm21751@gmail.com",
-    loginid:false,
-    branch:"Computer SCience",
-    year:3,
-    collegeNmae:"GITM LUCKNOW"
+// const jsUser={
+//     name:"sarita",
+//     age:22,
+//     email:"sm21751@gmail.com",
+//     loginid:false,
+//     branch:"Computer SCience",
+//     year:3,
+//     collegeNmae:"GITM LUCKNOW"
 
-}
+// }
 
-console.log(jsUser.name);
-console.log(jsUser["email"]);
-console.log(jsUser);
+// console.log(jsUser.name);
+// console.log(jsUser["email"]);
+// console.log(jsUser);
+
+//singleton
+
+//const tinderUser=new Object()
+
+const tinderUser={}
+
+tinderUser.id="123abc"
+tinderUser.name="janny"
+tinderUser.isloggedIn=false
+console.log(tinderUser);
+
+
+
+
+
