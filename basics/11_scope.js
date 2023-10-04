877326cce4e136070832985459aeb1781dbeed47
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-10-04 09:56:10 +0000

    scope and function

--- basics/11_scope.js
+++ basics/11_scope.js
@@ -5,9 +5,64 @@ if(true){
     let a=10
     const b=29
     var c=100
-    console.log("inner:",a);
+    //console.log("inner:",a);
 
 }
-console.log(a);
+//console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+
+function one(){
+     const username="sarita"
+     
+    function two(){
+        const website="github.com"
+        console.log(username);
+
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+
+
+
+
+if(true){
+    const username="sarita"
+    if(username==="sarita"){
+        const website=" google.com"
+        console.log(username + website);
+    }
+    //console.log(website);
+}
+console.log(username);
+
+
+//**************    ***************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
