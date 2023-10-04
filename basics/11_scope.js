b4528a0a34a9270a63460eb2841ee9ba1b75d05f
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-10-04 09:25:50 +0000

    scope

--- basics/11_scope.js
+++ basics/11_scope.js
@@ -1,10 +1,13 @@
 let a=50
+//var c=80
+//const b=60
 if(true){
     let a=10
     const b=29
     var c=100
+    console.log("inner:",a);
 
 }
 console.log(a);
-//console.log(b);
-//console.log(c);
+// console.log(b);
+// console.log(c);
