d2c41a9f87cad183b49f0336e7c845d2c7a7d4b3
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-10-05 05:16:36 +0000

    INVOKE FUNCTION

--- basics/12_invoke.js
+++ basics/12_invoke.js
@@ -1,5 +1,12 @@
 // Immediate Invoked function Expresssion (IIFE)
 
 (function login(){
+    //names IIFE
     console.log("db connected");
-})()
+})();
+
+
+( (name) =>{
+    console.log(`DB CONNECTED ${name}`);
+
+} )('sarita');
