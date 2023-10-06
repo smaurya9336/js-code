e7d63a6f9f1c64e3f5f16f9dcfa6bc44b26df76d
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-10-06 05:33:06 +0000

    loops

---
+++ basics/loops.js
@@ -0,0 +1,18 @@
+//ITERATION
+// for (let i = 1; i <= 10; i++) {
+//     const element = i;
+//     if (element==5) {
+//         console.log("5 is best");
+        
+//     }
+//     console.log(element);
+// }
+
+for (let i = 0; i < 10; i++) {
+    console.log(`outer loop ${i}`);
+    for (let j = 0; j < 10; j++) {
+        console.log(`inner loop ${j} and inner loop ${i}`);
+    }
+    
+    
+}
