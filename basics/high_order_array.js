451471ed6935d5ec99696da5f3859eaa8e58c476
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2023-10-06 06:25:18 +0000

    high order array loop

--- basics/high_order_array.js
+++ basics/high_order_array.js
@@ -1,7 +1,32 @@
 //for of loop
 
-const arr=[1,3,4,5]
-for (const num of arr) {
-    console.log(num);
+// const arr=[1,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+const greeting="Hello World"
+for (const greet of greeting) {
+    console.log(`Each char is ${greet}`);
     
 }
+
+
+// Maps
+
+const map=new Map()
+map.
+
+
+
+
+
+
+
+
+
+
+
+
+
